c97ca82f7fa831a312384f88b3e81263da61b4d7
Phoshoko Thabang <Phoshokothabang1@gmail.com>
2022-08-10 21:09:33 +0000

    simple shell

--- main_file.c
+++ main_file.c
@@ -1,10 +1,5 @@
 #include "main.h"
 
-	char **commands = NULL;
-	char *line = NULL;
-	char *shell_name = NULL
-	int status = 0;
-
 /**
  * main - the main shell code
  * @argc: number of arguments passed
